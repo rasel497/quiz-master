c3ca444b7bfbd3c41d3941a40fc12a4d322b6925
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-11 02:07:37 +0600

    react router setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Header from './componants/Header/Header';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Header></Header>
+    }
+  ])
   return (
     <div className="App">
       <h2>Hello React</h2>
+
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
