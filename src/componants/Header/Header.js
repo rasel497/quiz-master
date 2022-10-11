070a0a98286912823a2ba395155a13e1176f93a9
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-11 11:58:29 +0600

    bootstrap nav customise done

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div className='quiz'>
-                <h2>Quiz-Master</h2>
-            </div>
-            <nav className='header-link'>
-                <Link to='/'>Home</Link>
-                <Link to='/topics'>Topics</Link>
-                <Link to='/statics'>Statics</Link>
-                <Link to='/blog'>Blog</Link>
-                <Link to='/about'>About</Link>
-            </nav>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand to="/">Quiz-Master</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto"></Nav>
+                    <Nav className='nav-link'>
+                        <Link to="/">Home</Link>
+                        <Link to="/topics">Topics</Link>
+                        <Link to="/statics">Statics</Link>
+                        <Link to="/blog">Blog</Link>
+                        <Link to="/about">About</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
